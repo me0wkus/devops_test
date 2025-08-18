5b09094f25bf81a8808c6230ec9265294a6a8522
Vladislav <me0wkus@mail.ru>
2025-08-18 15:46:41 +0300

    Changed logger

--- app/logger.py
+++ app/logger.py
@@ -6,10 +6,11 @@ os.makedirs('./logs', exist_ok=True)
 
 # Настраиваем логирование
 logging.basicConfig(
-    level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.DEBUG,
     handlers=[
-        logging.FileHandler('./logs/app.log'),
+        logging.FileHandler('debug.log'),
         logging.StreamHandler()
     ]
 )
